be8e7074a512d97133a2b7a2a44f3349d4f0da53
Jignesh <jigneshmer.techngl@gmail.com>
2021-05-21 14:40:40 +0530

    intigrate vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,37 @@
 require('./bootstrap');
-
 window.Vue = require('vue');
+ 
+import App from './components/App.vue';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import {routes} from './routes';
+ 
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+ 
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+ 
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App),
+});
 
-// import App from './components/App.vue';
+// const files = require.context("./", true, /\.vue$/i);
+// files.keys().map(key =>
+//     Vue.component(
+//         key
+//             .split("/")
+//             .pop()
+//             .split(".")[0],
+//         files(key).default
+//     )
+// );
 
 // const app = new Vue({
-//   el: '#app',
-//   components: {
-//     App
-//   },
-//   render: h => h(App)
+//     el: "#app"
 // });
-
-
-const files = require.context("./", true, /\.vue$/i);
-files.keys().map(key =>
-    Vue.component(
-        key
-            .split("/")
-            .pop()
-            .split(".")[0],
-        files(key).default
-    )
-);
-
-const app = new Vue({
-    el: "#app"
-});
